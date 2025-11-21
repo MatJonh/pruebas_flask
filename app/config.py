35ed4b24972e1972d5151497f0e17da23125c0a5
MateoJuan <mateo51804084@gmail.com>
2025-11-20 22:53:00 -0600

    comexion de BD altera

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:6288@localhost:5432/flask_db"
+        "postgresql://postgres:6288@localhost:5432/flask"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
